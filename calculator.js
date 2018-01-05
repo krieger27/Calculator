2401af82f0dcf98053b5de6481a632e8daa645e8
krieger27 <krieger@vols.utk.edu>
2018-01-05 14:05:27 -0600

    think I'm done may need to ask on instructions

--- calculator.js
+++ calculator.js
@@ -22,10 +22,13 @@ function add(num1, num2) {
 Create a function that subtracts two numbers
 passed in as arguments. Return the difference.
 */
-function subtract(num1, num2) {
-  return num1-num2
-}
+// function subtract(num1, num2) {
+//   return num1-num2
+// } ----This is wrong had to reread instructions
 
+function calcModulo(num1, num2) {
+  return num1 % num2;
+}
 
 /*
 Create a function that divides two numbers
@@ -35,7 +38,6 @@ function divide(num1, num2){
   return num1/num2
 }
 
-
 /*
 Create a function that accepts three arguments.
   1. First number
@@ -44,3 +46,13 @@ Create a function that accepts three arguments.
 
 Return the value of the operation.
 */
+function calculate(num1, num2, func) {
+  return func(num1, num2);
+}
+
+let num1Input = document.getElementById("number");
+let num2Input = document.getElementById("number2");
+
+function printToDOM(value) {
+  document.getElementById("output").innerHTML = value;
+}
