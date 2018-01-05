de2b02dc21630d0a58bd1343376f6d195bc0edb4
krieger27 <krieger@vols.utk.edu>
2018-01-05 13:55:45 -0600

    basic functions setup

---
+++ calculator.js
@@ -0,0 +1,46 @@
+console.log("this is connected!!");
+/*
+Create a function that multiplies two numbers
+passed in as arguments. Return the product.
+*/
+
+
+function multiply(num1, num2) {
+  return num1 * num2
+};
+
+
+/*
+Create a function that adds two numbers
+passed in as arguments. Return the sum.
+*/
+function add(num1, num2) {
+  return num1 + num2
+};
+
+/*
+Create a function that subtracts two numbers
+passed in as arguments. Return the difference.
+*/
+function subtract(num1, num2) {
+  return num1-num2
+}
+
+
+/*
+Create a function that divides two numbers
+passed in as arguments. Return the quotient.
+*/
+function divide(num1, num2){
+  return num1/num2
+}
+
+
+/*
+Create a function that accepts three arguments.
+  1. First number
+  2. Second number
+  3. A function that performs an operation on them
+
+Return the value of the operation.
+*/
